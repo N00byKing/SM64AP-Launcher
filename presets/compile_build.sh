61bdb3d8ae9897d0c55fad38fbb55723b1f4c6f7
jacobmix <jacobmix@users.noreply.github.com>
2025-11-28 16:41:16 +0100

    Force MSYS2 compilers (#7)

--- presets/compile_build.sh
+++ presets/compile_build.sh
@@ -3,6 +3,10 @@
 
 FULL_BUILD_PATH="${BUILD_TARGET_DIR}/${BUILD_NAME}"
 
+# Force MSYS2 compilers
+export CC=/mingw64/bin/gcc
+export CXX=/mingw64/bin/g++
+
 cp  "${BUILD_ROM}" "${FULL_BUILD_PATH}/baserom.${BUILD_REGION}.z64"
 cd "${FULL_BUILD_PATH}"
 make ${BUILD_MAKE_FLAGS} VERSION="${BUILD_REGION}"
